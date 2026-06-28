0c12452ebc3efcb4f3c5a72a303ba6404143a6d2
manaskanugo97-max <manaskanugo97@gmail.com>
2026-06-28 22:54:43 +0530

    config.py

---
+++ config.py
@@ -0,0 +1,6 @@
+import os
+
+PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
+DATA_DIR = os.path.join(PROJECT_DIR, "data")
+
+FINAL_CSV = os.path.join(DATA_DIR, "healthcare_leads_final.csv")
